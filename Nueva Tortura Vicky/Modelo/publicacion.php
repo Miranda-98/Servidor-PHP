104bfccde66345e8d495d220087442d3623c147f
Miranda-98 <gerardomiranda2498@gmail.com>
2023-02-03 05:32:46 +0100

    la mamalona

--- Nueva Tortura Vicky/Modelo/publicacion.php
+++ Nueva Tortura Vicky/Modelo/publicacion.php
@@ -82,7 +82,7 @@
             */
             if(isset($_GET['pgnActual'])){
                 if($_GET['pgnActual']==1){
-                    header("Location:../Vista/paginaPublicaciones.php");
+                    header("Location:paginaPublicaciones.php");
                 } else {
                     $paginaActual = $_GET['pgnActual'];
                 }
@@ -90,79 +90,116 @@
                 $paginaActual = 1;
             }
 
-             // formula para la páginacino
+            // formula para la páginacino
             $paginacion = ($paginaActual-1) * $limite;
 
-            // sql para obtener los registros
-            //$sqlPaginacion = "SELECT * FROM viviendas LIMIT $paginacion,$limite";
-            
-            // sql con fotos
-            /* 
-            $sql = "SELECT viviendas.id as total_id, viviendas.tipo, viviendas.zona, viviendas.direccion, viviendas.ndormitorios, viviendas.precio, 
-            viviendas.tamano, viviendas.extras, fotos.foto, viviendas.observaciones, viviendas.fecha_anuncio FROM " . self::$TABLA . " INNER JOIN fotos WHERE viviendas.id = fotos.id_vivienda
-            ORDER BY viviendas.fecha_anuncio DESC LIMIT $paginacion,$limite";
+
+            /*  
+                SELECT viviendas.*, GROUP_CONCAT(fotos.foto) AS fotos
+                    FROM viviendas
+                    LEFT OUTER JOIN fotos ON viviendas.id = fotos.id_vivienda
+                    GROUP BY viviendas.id;
             */
+            $sql = "SELECT viviendas.id as total_id, tipo, zona, direccion, ndormitorios, precio, tamano, extras, observaciones,
+                     fecha_anuncio, GROUP_CONCAT(fotos.foto SEPARATOR ',') AS fotos
+                    FROM " . self::$TABLA . " LEFT OUTER JOIN fotos on viviendas.id = fotos.id_vivienda GROUP BY viviendas.id
+                    LIMIT $paginacion,$limite";
 
-            // sql sin fotos
-            $sql = "SELECT viviendas.id as total_id, viviendas.tipo, viviendas.zona, viviendas.direccion, viviendas.ndormitorios, viviendas.precio, 
-            viviendas.tamano, viviendas.extras, viviendas.observaciones, viviendas.fecha_anuncio FROM " . self::$TABLA . " ORDER BY viviendas.fecha_anuncio DESC LIMIT $paginacion,$limite";
+            $sqlCantidad = "SELECT COUNT(*) as cantidad FROM (SELECT viviendas.*, GROUP_CONCAT(fotos.foto) AS fotos
+              FROM viviendas LEFT OUTER JOIN fotos ON viviendas.id = fotos.id_vivienda GROUP BY viviendas.id ) AS subconsulta";
 
-            //obtener todas las publicaciones
-            // SELECT * FROM viviendas
-	        // INNER JOIN fotos WHERE viviendas.id = fotos.id_vivienda
-            // $sql = "SELECT id, tipo, zona, direccion, ndormitorios, precio, tamano, extras, observaciones, fecha_anuncio FROM ".self::$TABLA;
-            /*$sql = "SELECT viviendas.id as total_id, viviendas.tipo, viviendas.zona, viviendas.direccion, viviendas.ndormitorios, viviendas.precio, 
-                viviendas.tamano, viviendas.extras, fotos.foto, viviendas.observaciones, viviendas.fecha_anuncio FROM " . self::$TABLA . " INNER JOIN fotos WHERE viviendas.id = fotos.id_vivienda
-                ORDER BY viviendas.fecha_anuncio DESC";*/
+
             $publicaciones = $cone->query($sql);
 
             // en num se guardan la cantidad de registros para luego crear la cantidad de páginas necesarioas
-            $nRegistros = $cone->query($sql);
+            $nRegistros = $cone->query($sqlCantidad);
             $num = $nRegistros->fetch();
 
-            foreach($publicaciones as $fila) {
-                $idSeleccionado = $fila['total_id'];
-
-                echo "<tr>
-                        <td><button id='borrar'>BORRAR</button></td>
-                        <td><a href='../Controlador/cont_publicaciones.php?id=$idSeleccionado&valor=borrar'>Borrar</a><br/><a href=''>Modificar</a></td>
-                        <td>" . $fila['total_id'] . "</td>
-                        <td>" . $fila['tipo'] . "</td>
-                        <td>" . $fila['zona'] . "</td>
-                        <td>" . $fila['ndormitorios'] . "</td>
-                        <td>" . $fila['precio'] . "</td>
-                        <td>" . $fila['tamano'] . "</td>
-                        <td>" . $fila['extras'] . "</td>";
-                        // <td> <img src=../img/" . $fila['foto'] ."></td>
-                        echo "<td>" . $fila['observaciones'] . "</td>
-                        <td>" . $fila['fecha_anuncio'] . "</td>
+            echo "<style>table,tr,td{border:solid black 1px}img{width: 40px; height: 40px}td{width:100px;}</style>
+                <table>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>id</td>
+                        <td>tipo</td>
+                        <td>zona</td>
+                        <td>nDormitorios</td>
+                        <td>precio</td>
+                        <td>tamaño</td>
+                        <td>extras</td>
+                        <td>foto</td>
+                        <td>observaciones</td>
+                        <td>fecha anuncio</td>
                     </tr>";
+
+
+                
+
+                    foreach($publicaciones as $fila) {
+                    
+
+                    $idSeleccionado = $fila['total_id'];
+
+                 
+                    $aFotos = explode(',',$fila['fotos']);
+                    
+                    echo "<tr>
+                            <td><button id='borrar'>BORRAR</button></td>
+                            <td><a href='../Controlador/cont_publicaciones.php?id=$idSeleccionado&valor=borrar'>Borrar</a><br/><a href=''>Modificar</a></td>
+                            <td>" . $fila['total_id'] . "</td>
+                            <td>" . $fila['tipo'] . "</td>
+                            <td>" . $fila['zona'] . "</td>
+                            <td>" . $fila['ndormitorios'] . "</td>
+                            <td>" . $fila['precio'] . "</td>
+                            <td>" . $fila['tamano'] . "</td>
+                            <td>" . $fila['extras'] . "</td>";
+                            echo "<td>"; 
+                            for($i=0;$i<count($aFotos);$i++){
+                                echo "<a href='../img/".$aFotos[$i]."'>" . $aFotos[$i] . "</a><br/>";
+                            }
+                            echo "</td>";
+                            echo "<td>" . $fila['observaciones'] . "</td>
+                            <td>" . $fila['fecha_anuncio'] . "</td>
+                        </tr>";    
+   
             }
 
-            /*
+            echo "</table>";
+
+             /*
                 crear las páginas según la cantidad de registros existentes y la cantidad
                 de datos a mostrar por página, el ceil redondea a la alza(para que cuando
                 no se lleguen a la cantidad de registos minimo por pagina se muestren los
                 sobrantes para no perderlos)
                 el if es pura mariconada para estetica
             */
-            for ($i=1; $i <= ceil($num['total_id']/$limite); $i++) { 
-                if($i<ceil($num['total_id']/$limite))
+            for ($i=1; $i <= ceil($num['cantidad']/$limite); $i++) { 
+                if($i<ceil($num['cantidad']/$limite))
                     echo "<a href='?pgnActual=".$i."'>".$i."</a> - ";
                 else 
                     echo "<a href='?pgnActual=".$i."'>".$i."</a> ";
-            } 
-
+            }  
 
         }
 
         function filtrarPublicaciones()
         {
             try{
-                $sql = '-';
+                
+                /*
+                    la mamalona
+
+                    (SELECT viviendas.*, GROUP_CONCAT(fotos.foto) AS fotos
+                    FROM viviendas LEFT OUTER JOIN fotos ON viviendas.id = fotos.id_vivienda GROUP BY viviendas.id )
+                */
+
+                $sql = "SELECT viviendas.id as total_id, tipo, zona, direccion, ndormitorios, precio, tamano, extras, observaciones,
+                     fecha_anuncio, GROUP_CONCAT(fotos.foto SEPARATOR ',') AS fotos
+                    FROM " . self::$TABLA . " LEFT OUTER JOIN fotos on viviendas.id = fotos.id_vivienda";
 
                 if(isset($_GET['buscar'])) {
-                    $sql = "SELECT * FROM viviendas WHERE ";
+                    $sql = $sql . " WHERE";
+
                     //recoger el tipo de piso del formulario
                     if(isset($_GET['tipo']))
                         $sql = $sql . " tipo = '" . $_GET['tipo'] ."'";
@@ -189,7 +226,7 @@
                     //recoger los extras del formulario
                     if(!isset($_GET['garage']) && !isset($_GET['piscina']) && !isset($_GET['jardin'])){
                         echo "NO HAY EXTRAS";
-        
+                        $sql = $sql . "  GROUP BY viviendas.id";
                     } else {
                         $ex = '';
                         if(isset($_GET['piscina'])){
@@ -202,14 +239,21 @@
                             $ex = $ex . "". $_GET['jardin'];
                         }
 
-                        if(isset($_GET['garage']) && (isset($_GET['piscina']) || isset($_GET['jardin'])) || isset($_GET['garage'])){
+                        if (isset($_GET['piscina']) && isset($_GET['garage'])) {
+                            $ex = $ex . "," . $_GET['garage'];
+                        } else if (isset($_GET['jardin']) && isset($_GET['garage'])) {
                             $ex = $ex . "," . $_GET['garage'];
                         } else {
-                            $ex = $ex . "". $_GET['garage'];
+                            $ex = $ex . "" . $_GET['garage'];
                         }
+                        // if(isset($_GET['garage']) && (isset($_GET['piscina']) && isset($_GET['jardin'])) || isset($_GET['garage'])){
+                        //     $ex = $ex . "," . $_GET['garage'];
+                        // } else {
+                        //     $ex = $ex . " ". $_GET['garage'];
+                        // }
                     
                         $sql = $sql . " AND extras = '$ex";
-                        $sql = $sql . "'";
+                        $sql = $sql . "'  GROUP BY viviendas.id";
                     }
                 
                 }
@@ -219,38 +263,55 @@
             $c = new Conexion('inmobiliaria');
             $conecta = $c->conectar();
             $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo $sql;
+            echo "<br/><br/>".$sql;
             $result = $conecta->query($sql);
         
         
-            echo "<table border=solid black 1px>
-                <th colspan=11>TABLA CLIENTE</th>
-                            <tr>
-                                <td>ID</td>
-                                <td>TIPO</td>
-                                <td>ZONA</td>
-                                <td>DIRECCION</td>
-                                <td>DORMITORIOS</td>
-                                <td>PRECIO</td>
-                                <td>TAMAÑO</td>
-                                <td>EXTRAS</td>
-                                <td>OBSERVACIONES</td>
-                                <td>FECHA ANUNCIO</td>
-                            </tr>";
+            echo "<style>table,tr,td{border:solid black 1px}img{width: 40px; height: 40px}td{width:100px;}</style>
+                <table>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td>id</td>
+                        <td>tipo</td>
+                        <td>zona</td>
+                        <td>nDormitorios</td>
+                        <td>precio</td>
+                        <td>tamaño</td>
+                        <td>extras</td>
+                        <td>foto</td>
+                        <td>observaciones</td>
+                        <td>fecha anuncio</td>
+                    </tr>";
         
-            foreach ($result as $fila) {
-                echo " <tr>
-                    <td>" . $fila['id'] . "</td>",
-                    "<td>" . $fila['tipo'] . "</td>",
-                    "<td>" . $fila['zona'] . "</td>",
-                    "<td>" . $fila['direccion'] . "</td>",
-                    "<td>" . $fila['ndormitorios'] . "</td>",
-                    "<td>" . $fila['precio'] . "</td>",
-                    "<td>" . $fila['tamano'] . "</td>",
-                    "<td>" . $fila['extras'] . "</td>",
-                    "<td>" . $fila['observaciones'] . "</td>",
-                    "<td>" . $fila['fecha_anuncio'] . "</td></tr>";
-            }
+                    foreach($result as $fila) {
+                    
+
+                        $idSeleccionado = $fila['total_id'];
+
+                    
+                        $aFotos = explode(',',$fila['fotos']);
+                        
+                        echo "<tr>
+                                <td><button id='borrar'>BORRAR</button></td>
+                                <td><a href='../Controlador/cont_publicaciones.php?id=$idSeleccionado&valor=borrar'>Borrar</a><br/><a href=''>Modificar</a></td>
+                                <td>" . $fila['total_id'] . "</td>
+                                <td>" . $fila['tipo'] . "</td>
+                                <td>" . $fila['zona'] . "</td>
+                                <td>" . $fila['ndormitorios'] . "</td>
+                                <td>" . $fila['precio'] . "</td>
+                                <td>" . $fila['tamano'] . "</td>
+                                <td>" . $fila['extras'] . "</td>";
+                                echo "<td>"; 
+                                for($i=0;$i<count($aFotos);$i++){
+                                    echo "<a href='../img/".$aFotos[$i]."'>" . $aFotos[$i] . "</a><br/>";
+                                }
+                                echo "</td>";
+                                echo "<td>" . $fila['observaciones'] . "</td>
+                                <td>" . $fila['fecha_anuncio'] . "</td>
+                            </tr>";    
+   
+                    }
         
                 echo "</table>";
             } catch (PDOException $e) {
